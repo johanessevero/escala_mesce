c32f0b0050c53a27484e4ef7ca96a148103dc6dc
johanessevero <johanessevero@gmail.com>
2016-11-02 23:56:36 -0200

    interface

---
+++ escala_mesce/footer.php
@@ -0,0 +1,9 @@
+<div id="right-column"></div>
+</div>
+<div id="footer"></div>
+</div>
+<div align=center>
+	<a href='#'></a>
+</div>
+</body>
+</html>
---
+++ escala_mesce/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>ESCALA MESCE</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<style media="all" type="text/css">
+@import "css/all.css";
+</style>
+</head>
+<body>
+	<div id="main">
+		<div id="header">
+			<a href=""
+				class="logo"><img src="img/logo.gif" width="101" height="29" alt="" /></a>
+			<ul id="top-navigation">
+				<li class="active"><span><span>Home</span></span></li>
+			</ul>
+		</div>
+		<div id="middle">
+			<div id="left-column">
+				<h3></h3>
+				<ul class="nav">
+					<li><a href="cadastro_pessoa.php"> Cadastro de pessoas</a></li>
+					<li><a href="escolha_mes.php">+ Nova escala</a></li>
+				</ul>
+			</div>
+			<div id="center-column">
